9f879c258d14e7019d1ec2ee4910ca97eef7ad8f
ABBY <abbyW@moringa.com>
2022-04-24 16:45:24 +0300

    Added method for saving new user .

--- user.py
+++ user.py
@@ -7,9 +7,14 @@ class User:
 
     def __init__(self, username, password):
         """
-        __init__ method that defines login details of a user.
+        __init__ method that defines properties of user login object.
         """
         self.username = username
         self.password = password
-
+    def save_user(self):
+        """
+        method that saves a new user instance into the user list
+        """
+        User.user_list.append(self)
+    
     
