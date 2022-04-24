e188059fa01684217ceb99cc63234a39e5ec5ff8
ABBY <abbyW@moringa.com>
2022-04-24 21:08:05 +0300

    Created functions in the run.py file that enables user to create user account,save details of login, display list of user accounts saved  and verifies login.

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python3.8
 from user import User
 from credentials import Credentials
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
+
+def save_user(user):
+    '''
+    Function to save new user
+    '''
+    user.save_user()
+def display_users():
+    """
+    Function to display existing users
+    """
+    return User.display_user()
+def login_user(username,password):
+    """
+    function that verifies user is in userlist and asks for login details.
+    """
+  
+    check_user = Credentials.verify_user(username,password)
+    return check_user
