759235226e105a06d523d8e3afbbc348de9fd600
ABBY <abbyW@moringa.com>
2022-04-24 18:11:43 +0300

    Ran successful tests for initialization of credentials object and saving of user credentials in credentials list.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from user import User
+from credentials import Credentials
 
 
 class TestClass(unittest.TestCase):
@@ -51,6 +52,42 @@ class TestClass(unittest.TestCase):
             self.new_user.delete_user()# Deleting a user object
             self.assertEqual(len(User.user_list),1)
 
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+
+        """
+        self.new_credential = Credentials('Instagram','abigailnyawira','zy65h#QW')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Instagram')
+        self.assertEqual(self.new_credential.userName,'abigailnyawira')
+        self.assertEqual(self.new_credential.password,'zy65h#QW')
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
+
+
+    def test_save_credential(self):
+        """
+        test case to test if the crential object is saved into the credentials list.
+
+        """
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+   
 
 if __name__ == '__main__':
     unittest.main()
