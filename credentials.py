bd2481cf725e254ebad6e67faa0e2f9f2e9874dd
ABBY <abbyW@moringa.com>
2022-04-24 17:01:32 +0300

    Added method to verify whether user is in the existing list of users.

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+from user import User 
 import random
 import string
 import pyperclip
@@ -7,3 +8,13 @@ class Credentials():
     Class that generates new instances of user credentials
     """
     credentials_list = []
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify user in the list of users.
+        """
+        saved_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    saved_user == user.username
+        return saved_user
