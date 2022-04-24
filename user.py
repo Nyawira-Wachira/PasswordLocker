fdc95fc3e78549fbff37bee6f9b1c5865023611d
ABBY <abbyW@moringa.com>
2022-04-24 16:41:23 +0300

    Added method for  user login.

--- user.py
+++ user.py
@@ -7,27 +7,9 @@ class User:
 
     def __init__(self, username, password):
         """
-        method that defines login details of a user.
+        __init__ method that defines login details of a user.
         """
         self.username = username
         self.password = password
 
-    def save_user(self):
-        """
-        method that saves a new user instance into the user list
-        """
-        User.user_list.append(self)
     
-
-    @classmethod
-    def display_user(cls):
-        """
-        method that displays saved accounts of user
-        """
-        return cls.user_list
-
-    def delete_user(self):
-        '''
-        method that deletes a  saved account from the list
-        '''
-        User.user_list.remove(self)
